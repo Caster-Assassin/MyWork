f5128b0d186809c3bd104264e737c2b723c06826
十六夜 <1871145846@qq.com>
2019-12-26 17:13:18 +0800

    第二次提交

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("第一次");
+        System.out.println("1231234");
+        System.out.println("123456456456");
     }
 }
